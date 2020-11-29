b3481c17feebcf24ef309fc9558b9591ed262103
alejandro <alejitocastaneda1@gmail.com>
2020-11-29 16:18:36 +0100

    Igual que el anterior pero se me olvido SaldosCCDTO

---
+++ Ventanas/src/main/java/com/example/demo/dto/SaldoCCostosDTO.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto;
+
+import java.math.BigDecimal;
+
+import com.example.demo.model.pojos.SaldoCentroDeCostos;
+import com.example.demo.model.pojos.SaldoCuenta;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SaldoCCostosDTO {
+
+	@Include
+	private String cod_cuenta;
+	@Include
+	private Integer anno;
+	@Include
+	private Integer cCostos;
+	@Include
+	private String mes;
+	
+	private BigDecimal suma_debito;
+	private BigDecimal suma_credito;
+	private BigDecimal saldo_debito;
+	private BigDecimal saldo_credito;
+	
+	public SaldoCCostosDTO (SaldoCentroDeCostos s) {
+		
+		this(s.getElID().getCuenta().getCodCuenta(), s.getElID().getMes().getIdFiscal().getAnno().getElAnno(),s.getElID().getCentro().getCodCentro(), s.getElID().getMes().getIdFiscal().getMes(), s.getSumDebito(), s.getSumCredito(), s.getTotalDebito(), s.getTotalCredito());
+	}
+	
+}
